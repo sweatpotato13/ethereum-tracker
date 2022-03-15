54c1d72ec8a9df077522801cfaf0aa33d58fd216
Cute_Wisp <sweatpotato13@gmail.com>
2022-03-15 12:07:21 +0900

    feat: 🎸 add uniswap router abi

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,39 @@
 import * as dotenv from "dotenv";
 import { ethers } from "ethers";
+import abi from "../json/uniswap_v2_router_abi.json"
 dotenv.config();
-var url = process.env.INFURA_ENDPOINT;
+const url = process.env.INFURA_ENDPOINT;
 
-var init = function () {
-    var customWsProvider = new ethers.providers.WebSocketProvider(url);
+async function main() {
+    const customWsProvider = new ethers.providers.WebSocketProvider(url);
+    const contract = new ethers.Contract("0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D", abi, customWsProvider);
+    const iface = new ethers.utils.Interface(abi)
 
     customWsProvider.on("pending", (tx) => {
         customWsProvider.getTransaction(tx).then(function (transaction) {
-            if (transaction && transaction["to"] === "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D") {
-                console.log(transaction);
+            if (transaction && transaction["to"] == "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D") {
+                try {
+                    const data = iface.decodeFunctionData("swapExactETHForTokens", transaction.data);
+                    console.log(transaction);
+                    console.log(data);
+                } catch (error) {
+                    // DO NOTHING
+                }
             }
         });
     });
 
     customWsProvider._websocket.on("error", async () => {
         console.log(`Unable to connect, retrying in 3s...`);
-        setTimeout(init, 3000);
+        setTimeout(main, 3000);
     });
     customWsProvider._websocket.on("close", async (code) => {
         console.log(
             `Connection lost with code ${code}! Attempting reconnect in 3s...`
         );
         customWsProvider._websocket.terminate();
-        setTimeout(init, 3000);
+        setTimeout(main, 3000);
     });
-};
+}
 
-init();
+main();
